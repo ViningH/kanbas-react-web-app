2bd6fc702d5b99a4a4862b539443ccbb835a2b42
Hannah <vining.h@husky.neu.edu>
2024-03-12 00:22:36 -0400

    finish a4 lab

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -3,6 +3,14 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -16,6 +24,14 @@ const Assignment4 = () => {
       <PassingDataOnEvent />
       <PassingFunctions theFunction={sayHello} />
       <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
     </>
   );
 };
