563ea951168e257a27cf52ad5fcc3766fe6f5ba5
Hannah <vining.h@husky.neu.edu>
2024-03-13 23:00:15 -0400

    a4 undergrad part finished

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -3,9 +3,21 @@ import "./index.css";
 import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaExternalLinkAlt, FaLink } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./reducer";
+import { KanbasState } from "../../store";
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
+  const modulesList = useSelector((state: KanbasState) =>
+    state.modulesReducer.modules);
+  const module = useSelector((state: KanbasState) =>
+    state.modulesReducer.module);
+  const dispatch = useDispatch();
   const [selectedModule, setSelectedModule] = useState(modulesList[0]);
   return (
     <>
@@ -21,47 +33,70 @@ function ModuleList() {
       </div>
       <hr />
       <ul className="list-group wd-modules">
-        {modulesList.map((module) => (
-          <li
-            className="list-group-item"
-            onClick={() => setSelectedModule(module)}>
-            <div>
-              <FaEllipsisV className="me-2" />
-              {module.name}
-              <span className="float-end">
-                <FaCheckCircle className="text-success" />
-                <FaPlusCircle className="ms-2" />
-                <FaEllipsisV className="ms-2" />
-              </span>
-            </div>
-            {selectedModule._id === module._id && (
-              <ul className="list-group">
-                {module.lessons?.map((lesson) => (
-                  
-                  <li className="list-group-item "  >
-                    <FaEllipsisV className="me-2" />
-                    {lesson.extlink? 
-                    <>
-                    <FaLink className="me-2 text-success" />
-                    <a href={lesson.extlink} className="wd-red-link">
-                      {lesson.name} <FaExternalLinkAlt className="me-2" />
-                      </a></> : lesson.name}
-                    <span className="float-end">
-                      <FaCheckCircle className="text-success" />
-                      <FaEllipsisV className="ms-2" />
-                    </span>
+        <li className="list-group-item">
+          <input value={module.name}
+            onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))
+            }
+          /><br />
+          <textarea value={module.description}
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))
+            }
+          />
+          <button onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+          <button onClick={() => dispatch(updateModule(module))}>
+            Update
+          </button>
+        </li>
+        {modulesList.filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <li key={index}
+              className="list-group-item"
+              onClick={() => setSelectedModule(module)}>
+              <div>
+                <FaEllipsisV className="me-2" />
+                {module.name}
+                <span className="float-end">
+                  <button
+                    onClick={() => dispatch(deleteModule(module._id))}>
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => dispatch(setModule(module))}>
+                    Edit
+                  </button>
+                  <FaCheckCircle className="text-success" />
+                  <FaPlusCircle className="ms-2" />
+                  <FaEllipsisV className="ms-2" />
+                </span>
+              </div>
+              {selectedModule._id === module._id && (
+                <ul className="list-group">
+                  {module.lessons?.map((lesson: any) => (
 
-                  </li>
-                  
-                )
-                
-                )
-                }
-                
-              </ul>
-            )}
-          </li>
-        ))}
+                    <li className="list-group-item "  >
+                      <FaEllipsisV className="me-2" />
+                      {lesson.extlink ?
+                        <>
+                          <FaLink className="me-2 text-success" />
+                          <a href={lesson.extlink} className="wd-red-link">
+                            {lesson.name} <FaExternalLinkAlt className="me-2" />
+                          </a></> : lesson.name}
+                      <span className="float-end">
+                        <FaCheckCircle className="text-success" />
+                        <FaEllipsisV className="ms-2" />
+                      </span>
+
+                    </li>
+
+                  )
+
+                  )
+                  }
+
+                </ul>
+              )}
+            </li>
+          ))}
       </ul>
     </>
   );
