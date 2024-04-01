8d451ffe08e50073e2e86bf5f23f71b28f0bb149
Hannah <vining.h@husky.neu.edu>
2024-04-01 02:08:22 -0400

    did undergrad portion

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
-import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaExternalLinkAlt, FaLink } from "react-icons/fa";
 import { useParams } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
@@ -9,7 +8,9 @@ import {
   deleteModule,
   updateModule,
   setModule,
+  setModules,
 } from "./reducer";
+import * as client from "./client";
 import { KanbasState } from "../../store";
 function ModuleList() {
   const { courseId } = useParams();
@@ -18,6 +19,26 @@ function ModuleList() {
   const module = useSelector((state: KanbasState) =>
     state.modulesReducer.module);
   const dispatch = useDispatch();
+  useEffect(() => {
+    client.findModulesForCourse(courseId)
+      .then((modules) =>
+        dispatch(setModules(modules))
+      );
+  }, [courseId]);
+  const handleAddModule = () => {
+    client.createModule(courseId, module).then((module) => {
+      dispatch(addModule(module));
+    });
+  };
+  const handleDeleteModule = (moduleId: string) => {
+    client.deleteModule(moduleId).then((status) => {
+      dispatch(deleteModule(moduleId));
+    });
+  };
+  const handleUpdateModule = async () => {
+    const status = await client.updateModule(module);
+    dispatch(updateModule(module));
+  };
   const [selectedModule, setSelectedModule] = useState(modulesList[0]);
   return (
     <>
@@ -46,8 +67,8 @@ function ModuleList() {
         />
         <div className="wd-align-right">
           <button className="wd-add-button"
-            onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
-          <button className="wd-edit-button" onClick={() => dispatch(updateModule(module))}>
+            onClick={handleAddModule}>Add</button>
+          <button className="wd-edit-button" onClick={handleUpdateModule}>
             Update
           </button>
         </div>
@@ -63,7 +84,7 @@ function ModuleList() {
                 {module.name}
                 <span className="float-end">
                   <button className="wd-red-delete-button"
-                    onClick={() => dispatch(deleteModule(module._id))}>
+                    onClick={() => handleDeleteModule(module._id)}>
                     Delete
                   </button>
                   <button className="wd-edit-button"
@@ -75,7 +96,7 @@ function ModuleList() {
                   <FaEllipsisV className="ms-2" />
                 </span>
               </div>
-              {selectedModule._id === module._id && (
+              {selectedModule && selectedModule?._id === module._id && (
                 <ul className="list-group">
                   {module.lessons?.map((lesson: any) => (
 
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,4 @@
-import { courses } from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams} from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import "./index.css";
@@ -12,9 +11,22 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import CourseBreadcrumb from "./CourseBreadcrumb/breadcrumb";
 import CourseDetails from "./Settings/CourseDetails";
 import CourseSettingsNavigation from "./Settings/Navigation";
-function Courses({ courses }: { courses: any[]; }) {
+import { useState, useEffect } from "react";
+import axios from "axios";
+function Courses() {
     const { courseId } = useParams();
-    const course = courses.find((course) => course._id === courseId);
+    const COURSES_API = "http://localhost:4000/api/courses";
+    const [course, setCourse] = useState<any>({ _id: "" });
+    const findCourseById = async (courseId?: string) => {
+        const response = await axios.get(
+            `${COURSES_API}/${courseId}`
+        );
+        setCourse(response.data);
+    };
+    useEffect(() => {
+        findCourseById(courseId);
+    }, [courseId]);
+
     return (
         <div className="wd-main-content">
             <CourseBreadcrumb course={course} />
