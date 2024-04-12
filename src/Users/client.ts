c4e62d76b9c11b4a1a8340b04d5836114aa61356
Hannah <vining.h@husky.neu.edu>
2024-04-12 15:42:10 -0400

    more a6

--- src/Users/client.ts
+++ src/Users/client.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
+export const api = axios.create({
+    withCredentials: true
+});
 export interface User {
     _id: string; username: string; password: string; role: string;
     firstName: string, lastName: string
 };
 export const signin = async (credentials: User) => {
-    const response = await axios.post(`${USERS_API}/signin`, credentials);
+    const response = await api.post(`${USERS_API}/signin`, credentials);
     return response.data;
 };
 export const profile = async () => {
-    const response = await axios.post(`${USERS_API}/profile`);
+    const response = await api.post(`${USERS_API}/profile`);
     return response.data;
 };
 export const updateUser = async (user: any) => {
@@ -40,10 +43,10 @@ export const findUsersByRole = async (role: string) => {
     return response.data;
 };
 export const signup = async (user: any) => {
-    const response = await axios.post(`${USERS_API}/signup`, user);
+    const response = await api.post(`${USERS_API}/signup`, user);
     return response.data;
 };
 export const signout = async () => {
-    const response = await axios.post(`${USERS_API}/signout`);
+    const response = await api.post(`${USERS_API}/signout`);
     return response.data;
 };
